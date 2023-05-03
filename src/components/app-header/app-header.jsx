ea8ecbfa31ff38f943b4169250d2205377f4bf98
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-03 15:27:25 +0300

    commit 7

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
+
 import {Logo} from '@ya.praktikum/react-developer-burger-ui-components';
 import {BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ListIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import {ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
 
 function AppHeader(props) {
   return (
-    <div>
-      <BurgerIcon type="primary"/>
-      <p>Конструктор</p>
-      <ListIcon type="secondary" />
-      <p>Лента заказов</p>
-      <Logo />
-      <ProfileIcon type="secondary" />
-      <p>Личный кабинет</p>
-    </div>
+    <nav className={styles.header}>
+      <ul className={styles.headerlist}>
+        <li className={styles.listelement}>
+          <div>
+          <BurgerIcon type="primary"/>
+          </div>
+          <p className={styles.pharagraph}>Конструктор</p>
+        </li>
+        <li className={styles.listelement}>
+          <div>
+          <ListIcon type="secondary"/>
+          </div>
+          <p className={styles.pharagraph}>Лента заказов</p>
+        </li>
+        <li className={styles.logopicture}>
+          <div className={styles.logo}>
+            <Logo />
+          </div>
+        </li>
+        <li className={styles.listelement}>
+          <div>
+            <ProfileIcon type="secondary" />
+          </div>
+          <p className={styles.pharagraph}>Личный кабинет</p>
+        </li>
+      </ul>      
+      
+    </nav>
   );
 }
 
