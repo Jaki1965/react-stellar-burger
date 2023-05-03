e2b0ea177b1b6101380a468ee4d9aa33c6a4a943
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-04 01:01:36 +0300

    commit 10

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,18 @@
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import AppHeader from "../app-header/app-header";
-
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
   return (
     <div className={styles.app}>
-      <pre style={{
-      	fontSize: "1.5rem"
-      }}>
+      <pre style={{fontSize: "1.5rem"}}>
         <AppHeader />
-      	
+        <main className={styles.main}>
+          <BurgerIngredients data={data} />
+          <BurgerConstructor />
+        </main>
       </pre>
     </div>
   );
