08d30edb179a636f5285b73dbd472cea4eccf400
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-09 17:58:43 +0300

    commit 24

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,48 @@
 import styles from "./app.module.css";
-import { data } from "../../utils/data";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import React from 'react';
 
 function App() {
+
+  const urlData = 'https://norma.nomoreparties.space/api/ingredients';
+
+  const [stateData, setStateData] = React.useState([]);
+  const [isError, setIsError] = React.useState(false);
+
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+          const res = await fetch(urlData);
+          const burgerData = await res.json();
+          setStateData(burgerData.data);
+      }
+      catch (err) {
+          setIsError(true)
+          console.log('Ошибка загрузки данных', err)
+      }
+      }
+
+    getData();
+
+  }, []);
+
+  
+
+
   return (
     <div className={styles.app}>
         <AppHeader />
-        <main className={styles.main}>
-          <BurgerIngredients data={data} />
-          <BurgerConstructor data={data}/>
+        {isError ? <h2 className={styles.error}>Ошибка загрузки данных с сервера</h2> :
+         <main className={styles.main}>
+          {stateData.length && <BurgerIngredients data={stateData} />}
+          {stateData.length && <BurgerConstructor data={stateData}/>}
         </main>
+        }
     </div>
   );
 }
 
 export default App;
-
