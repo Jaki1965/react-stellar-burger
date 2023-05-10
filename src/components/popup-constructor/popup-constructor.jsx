153d5d59c50f2410dd997df838408c4d926fd5a3
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-10 11:04:21 +0300

    commit 29

--- src/components/popup-constructor/popup-constructor.jsx
+++ src/components/popup-constructor/popup-constructor.jsx
@@ -10,15 +10,19 @@ const PopupConstructor = ({onClose}) =>{
   return (
     <div>
       <ul className={styles.main}>
-        <CloseIcon type="primary" onClick={onClose}/>
+        <div className={styles.close_icon}>
+          <CloseIcon type="primary" onClick={onClose}/>
+        </div>
         <li className={styles.detail}>
-          <p className={`${styles.number} text text_type_digits-large pb-5`}>034536</p>
-          <p className="text text_type_main-medium pt-3">идентификатор заказа</p>
+          <p className={`${styles.number} text text_type_digits-large`}>034536</p>
+        </li>
+        <li className={styles.order}>
+          <p className="text text_type_main-medium">идентификатор заказа</p>
         </li>
         <li className={styles.detail}>
           <img src={checkIcon} alt='Иконка с галочкой'></img>
         </li>
-        <li className={styles.detail}>
+        <li className={styles.text}>
           <p className="text text_type_main-default">Ваш заказ начали готовить</p>
           <p className="text text_type_main-default text_color_inactive pt-2">Дождитесь готовности на орбитальной станции</p>
         </li>
