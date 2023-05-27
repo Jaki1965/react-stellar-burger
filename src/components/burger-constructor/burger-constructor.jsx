242d3c14d83fbfc833c9de4713ca23774b386c16
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-27 20:21:40 +0300

    commit 64

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -48,9 +48,29 @@ const {data} = useSelector(store => store.data);
     setIsPopupOpen(null)
   }
 
+
+
+  
+
+
   // const elementBurgerClosed = data.find(item => item.type === 'bun');
-  const elementBurgerClosed = data[0];
-  const elementBurger = data.filter(item => item.type !== 'bun');
+  // const elementBurgerClosed = data[0];
+const elementBurgerClosed = {
+    calories: 420,
+    carbohydrates: 53,
+    fat: 24,
+    image: "https://code.s3.yandex.net/react/code/bun-02.png",
+    image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+    image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+    name: "Краторная булка N-200i",
+    price: 1255,
+    proteins: 80,
+    type: "bun",
+    __v: 0,
+    _id: "643d69a5c3f7b9001cfa093c"
+}
+  
+const elementBurger = data.filter(item => item.type !== 'bun');
   
 
   //const finalPrice = elementBurger.reduce((sum, item) => { return sum + item.price},  elementBurgerClosed.price * 2);
