904ffa3be98303a0aa8f6703c45642e253022731
Jaki1965 <igor.kiryanov@mail.ru>
2023-05-30 13:52:09 +0300

    commit 70

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -9,13 +9,21 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getBurgerData } from '../services/actions/api';
 import {ITEM_OPEN, ITEM_CLOSE, MODAL_OPEN, MODAL_CLOSE} from '../services/actions/burger-ingredients';
+import { useInView } from "react-intersection-observer";
 
 function BurgerIngredients() {
 
   const {data} = useSelector(store => store.data);
   const open = useSelector(store => store.open.isOpen);
+
+  const [buns, bunsEntry] = useInView({ threshold: 1, root: document.querySelector('#viewport') });
+  const [sauces, saucesEntry] = useInView({ threshold: 0.9, root: document.querySelector('#viewport')});
+  const [fillings, fillingsEntry] = useInView({ threshold: 0.2, root: document.querySelector('#viewport')});
  
- 
+ console.log(bunsEntry, 'булка');
+ console.log(saucesEntry, 'соус');
+ console.log(fillingsEntry, 'начинка');
+
 
   const dispatch = useDispatch();
  
@@ -34,7 +42,7 @@ function BurgerIngredients() {
     dispatch({type: ITEM_CLOSE});
   }
 
-  const [current, setCurrent] = React.useState('rolls')
+  const [current, setCurrent] = React.useState()
 
   const bun = data.filter(item => item.type === 'bun');
   const sauce = data.filter(item => item.type === 'sauce');
@@ -46,28 +54,27 @@ function BurgerIngredients() {
     })
   }
 
- 
-
+  
   return (
     <>
       <section>
         <h1 className="text text_type_main-large mt-10 mb-5">Соберите бургер</h1>
         <div className={styles.menu}>
-          <Tab value='rolls' active={current === 'rolls'} onClick={setCurrent}>Булки</Tab>
-          <Tab value='sauces' active={current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
-          <Tab value='fillings' active={current === 'fillings'} onClick={setCurrent}>Начинки</Tab>
-        </div>
-        <div className={`custom-scroll ${styles.scrollbar}`}>
+          <Tab value='buns' active={bunsEntry === true || current === 'buns' } onClick={setCurrent}>Булки</Tab>
+          <Tab value='sauces' active={saucesEntry === true || current === 'sauces'} onClick={setCurrent}>Соусы</Tab>
+          <Tab value='fillings' active={fillingsEntry === true || current === 'fillings'} onClick={setCurrent}>Начинки</Tab>
+        </div >
+        <div id="viewport" className={`custom-scroll ${styles.scrollbar}`}>
           <h2 className="text text_type_main-medium mt-10 mb-6">Булки</h2>
-          <ul className={styles.list}>
+          <ul  ref={buns} className={styles.list}>
             {getDataCard(bun)}
           </ul>
-          <h2 className="text text_type_main-medium mt-10 mb-6">Соусы</h2>
-          <ul className={styles.list}>
+          <h2 className="text text_type_main-medium mt-10 mb-6" >Соусы</h2>
+          <ul ref={sauces} className={styles.list}>
             {getDataCard(sauce)}
           </ul>
-          <h2 className="text text_type_main-medium mt-10 mb-6">Начинки</h2>
-          <ul className={styles.list}>
+          <h2 className="text text_type_main-medium mt-10 mb-6" >Начинки</h2>
+          <ul ref={fillings} className={styles.list}>
             {getDataCard(main)}
           </ul>
         </div>
