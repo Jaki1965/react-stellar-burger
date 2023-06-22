3fe6459791d698dcf07348242d6f639a6ea11414
Jaki1965 <igor.kiryanov@mail.ru>
2023-06-22 10:31:58 +0300

    commit 98

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,7 +7,7 @@ import Login from '../../pages/login/login';
 import Home from "../../pages/home/home";
 import Register from "../../pages/register/register";
 import ForgotPassword from "../../pages/forgot-password/forgot-password";
-
+import ResetPassword from "../../pages/reset-password/reset-password";
 
 
 function App() {
@@ -21,6 +21,7 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
+          <Route path="/reset-password" element={<ResetPassword />}/>
         </Routes>     
       </DndProvider>
     </div>
---
+++ src/pages/reset-password/reset-password.jsx
@@ -0,0 +1,44 @@
+import styles from './reset-password.module.css';
+import {Link} from 'react-router-dom';
+import { Input, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useState, useRef } from 'react';
+
+  
+  function ResetPassword() {
+  
+    const [password, setPassword] = useState('');
+    const [code, setCode] = useState('');
+    const inputRef = useRef(null);
+
+    return (
+      <>
+        <section className={styles.reset_password}>
+          <form className={styles.reset_password_form}>
+            <h2 className="text text_type_main-medium">Восстановление пароля</h2>
+            <PasswordInput
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                name={'password'}
+                placeholder={'Введите новый пароль'}
+            />
+            <Input
+                type={'text'}
+                placeholder={'Введите код из письма'}
+                onChange={e => setCode(e.target.value)}
+                value={code}
+                name={'securityCode'}
+                error={false}
+                ref={inputRef}
+                errorText={'Ошибка'}
+            />
+            <Button htmlType="submit" type="primary" size="large" disabled={!code || !password}>Сохранить</Button>
+          </form>
+          <span className="text text_type_main-default text_color_inactive pt-20">
+            Вспомнили пароль? <Link to="/login" className={styles.reset_password_link}>Войти</Link>
+          </span>
+        </section>
+      </>
+    )
+}
+  
+  export default ResetPassword;
